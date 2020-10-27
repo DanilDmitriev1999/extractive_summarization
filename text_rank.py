27e805dc9638fc287b8d90271862089459e66965
Данил <46111455+DanilDmitriev1999@users.noreply.github.com>
2020-10-27 19:06:27 +0300

    add hash element in class TextRank

--- text_rank.py
+++ text_rank.py
@@ -13,9 +13,9 @@ class TextRank:
     def __init__(self, model_name:str):
         self.encoder = SentenceTransformer(model_name)
 
-    def model_similarity(self, w1, w2):
-        u = np.mean(self.encoder.encode([' '.join(w1)]), axis=0)
-        v = np.mean(self.encoder.encode([' '.join(w2)]), axis=0)
+    def model_similarity(self, hash_vec, idx_1, idx_2):
+        u = hash_vec[idx_1]
+        v = hash_vec[idx_2]
 
         return self.cosine_sim(u, v)
 
@@ -28,9 +28,12 @@ class TextRank:
         sentences_words = [[token.text.lower() if lower else token.text for token in razdel.tokenize(sentence)] for
                            sentence in sentences]
 
+        # хешируем предложения и их вектора
+        hash_vec = {idx:np.mean(self.encoder.encode([' '.join(i)]), axis=0) for idx, i in enumerate(sentences_words)}
+
         # Для каждой пары предложений считаем близость
         pairs = combinations(range(n_sentences), 2)
-        scores = [(i, j, self.model_similarity(sentences_words[i], sentences_words[j])) for i, j in tqdm(pairs)]
+        scores = [(i, j, self.model_similarity(hash_vec, i, j)) for i, j in pairs]
 
         # Строим граф с рёбрами, равными близости между предложениями
         g = nx.Graph()
@@ -55,6 +58,7 @@ class TextRank:
 
     @staticmethod
     def calc_scores(references, predictions, text):
+        print()
         print("Count:", len(predictions))
         print('Полный текст:')
         pprint(text, width=150)
